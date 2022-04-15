e2262e8b00a78588f641687e908e7c476700b6f4
ervinanovlianti <ervinanov03@gmail.com>
2022-04-15 10:05:40 +0800

    portfolio-tailwindcss

---
+++ dist/js/script.js
@@ -0,0 +1,46 @@
+// Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+    const backTop = document.querySelector('#back-top')
+
+    // Jika window di scroll maka tambahkan navbar-fixed dan jika tidak maka kembalikan seperti semula.
+    if(window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        backTop.classList.remove('hidden');
+        backTop.classList.add('flex');
+    }else{
+        header.classList.remove('navbar-fixed');
+        backTop.classList.add('hidden');
+        backTop.classList.remove('flex');
+    }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+})
+
+// klik di luar hamburger
+window.addEventListener('click', function(e){
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//Darkmode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    }
+});
