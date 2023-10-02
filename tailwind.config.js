05566749d57bba48fccec9822f58ea8f7f5c621a
ervinanovlianti <ervinanov03@gmail.com>
2023-10-03 00:15:17 +0800

    update primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary : '#14b8a6',
+        primary: '#0d9488',
         secondary: '#64748b',
         dark : '#0f172a'
       },
